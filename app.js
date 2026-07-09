2aaaed7d5a4c90920da3d11d3d97dee1929aa144
Rehman <rehmanmohammad938@gmail.com>
2026-07-09 11:14:57 -0400

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { db } = require("./models") 
 const app = express();
+const taskRouter = require("./routes/tasks");
+const usersRouter = require("./routes/users");
 
 app.use(express.json());
 
@@ -8,11 +10,15 @@ app.get("/health", (req, res) => {
     res.json({ status: "ok"});
 });
 
+app.use("/api/tasks", taskRouter);
+
 db.sync().then(() => {
     app.listen(3000, () => {
         console.log("Server running on Port 3000");
     });
 });
+
+app.use("/api/users", usersRouter);
 
 
 
